78515be9143a98a56c52ab6c51d988135653b924
Charles <uyo@hotmail.fr>
2018-04-01 21:11:31 +0200

    First working version of select item menu

--- app/src/main/java/charles/courses/SelectItemActivity.java
+++ app/src/main/java/charles/courses/SelectItemActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,17 +17,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.RadioButton;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -57,7 +52,7 @@ public class SelectItemActivity extends AppCompatActivity {
     Input input_ = new Input();
     Result result_ = new Result();
     SelectItemAdapter adapter_ = null;
-    ListView listView_ = null;
+    RecyclerView listView_ = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,167 +79,43 @@ public class SelectItemActivity extends AppCompatActivity {
             }
         }
         //Display the choices
-        adapter_ = new SelectItemAdapter(this, new ArrayList<String>() );
+        adapter_ = new SelectItemAdapter(this );
         listView_ = findViewById(R.id.ItemsList);
+        listView_.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        listView_.setItemAnimator(new DefaultItemAnimator());
         listView_.setAdapter(adapter_);
 
-        //Initialize the return value
-        result_.selected_ = input_.selected_;
+        //Default result
         updateResult();
     }
 
-    static class SelectItemAdapter extends ArrayAdapter<String> {
-        ArrayList<String> items_;
-        SelectItemActivity activity_;
-        int selected_ = 0;
-        EditText editedText_;
+    @Override
+    public void finish() {
+        updateResult();
+        super.finish();
+    }
 
-        SelectItemAdapter(SelectItemActivity activity, ArrayList<String> displayItems) {
-            super(activity, 0, displayItems );
-            activity_ = activity;
-            items_ = displayItems;
-            items_.addAll( activity_.input_.values_);
-            for ( int i = 0; i < items_.size(); i++ ) {
-                if ( items_.get( i ).equals( activity_.input_.selected_ ) ) {
-                    selected_ = i;
-                }
+    void updateResult() {
+        //Compute the modified and added list
+        for ( int i = 0; i < input_.values_.size(); i++ ) {
+            String oldItem = input_.values_.get( i );
+            String newItem = adapter_.items_.get(i);
+            if ( !oldItem.equals(newItem) ) {
+                result_.modifiedItems_.add( new ValueChange( oldItem, newItem ) );
             }
         }
 
-        @Override
-        @NonNull
-        public View getView(int position, View convertView, @NonNull final ViewGroup parentView) {
-            View v = convertView;
-            final ListView listView = (ListView) parentView;
-            boolean mustCreate = v == null;
-            if (mustCreate) {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                v = inflater.inflate(R.layout.select_item, null);
-            }
-
-            //Check the appropriate selector
-            final RadioButton selector = v.findViewById(R.id.item_selector);
-            final ImageButton edit_button = v.findViewById(R.id.edit_item);
-            final ImageButton remove_button = v.findViewById(R.id.remove_item);
-            final EditText text = v.findViewById(R.id.item_text);
-            selector.setTag( position );
-            edit_button.setTag( position );
-            remove_button.setTag( position );
-            text.setTag(position);
-
-            //Check the selector
-            boolean isSelected = selected_ == position;
-            boolean selectionChange = selector.isChecked() != isSelected;
-            selector.setChecked( isSelected );
-
-            //Display the item
-            final String item = getItem( position );
-            if ( !text.getText().toString().equals( item ) ) {
-                text.setText(item);
-            }
-
-            System.out.println( "Getting view " + position + " for item " + item + " selectionchange=" + selectionChange + " selected = " + selected_ );
-            if ( item.isEmpty() && !text.hasFocus() ) {
-                System.out.println( "Requesting focus" );
-                editItem(text, listView);
-            }
-
-            //Edit the listener if it's the first initialization
-            if ( mustCreate ) {
-                selector.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        selected_ = (int) selector.getTag();
-                        notifyDataSetChanged();
-                    }
-                });
-
-                text.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                    }
-
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        items_.set((int) text.getTag(), s.toString());
-                    }
-                });
-                text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View v, boolean hasFocus) {
-                        //Detect if this is an actual focus change
-                        System.out.println( "Vue " + text.getText().toString() + ( hasFocus ? " gagne" : " perd") + " le focus" );
-                        if ( !hasFocus || editedText_ == text ) {
-                            return;
-                        }
-
-                        //If the view is too far down, we scroll to it before displaying the keyboard so that the view does not get destroyed
-                        int pos = (int) v.getTag();
-                        listView.setSelection(pos);
-                        editedText_ = text;
-                        //Wait for focus to be settled before opening keyboard
-                        text.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                System.out.println( "Asking to show the keyboard");
-                                //We show the keyboard
-                                InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                                imm.showSoftInput(text, InputMethodManager.SHOW_IMPLICIT);
-
-                                //Showing the keyboard will refresh the main view and lose the focus again : we request it again
-                                text.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        System.out.println( "Asking to refresh focus after keyboard");
-                                        text.requestFocus();
-                                    }
-                                }, 500);
-                            }
-                        }, 500);
-                    }
-                });
-                //Edit button : make it possible to edit items
-                edit_button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        editItem(text, (ListView) listView);
-                    }
-                });
-
-                //Remove button : make it possible to remove items
-                remove_button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        //Remove it from the display list
-                        int current_pos = (int) remove_button.getTag();
-                        items_.remove(current_pos);
-
-                        //Remove it from the original list and add it to the removed ones in the result
-                        if (activity_.input_.values_.size() > current_pos) {
-                            activity_.result_.removedItems_.add(activity_.input_.values_.get(current_pos));
-                            activity_.input_.values_.remove(current_pos);
-                        }
-                        notifyDataSetChanged();
-                    }
-                });
-            }
-            return v;
+        for ( int i = input_.values_.size(); i < adapter_.getItemCount(); i++ ) {
+            result_.addedItems_.add( adapter_.items_.get(i));
         }
+        result_.selected_ = adapter_.items_.get( adapter_.selected_ );
 
-        void editItem( final EditText item, final ListView list ) {
-            item.postDelayed( new Runnable() {
-                @Override
-                public void run() {
-                    editedText_ = null;
-                    item.requestFocus();
-                    item.setSelection(item.getText().length());
-                }
-            }, 200);
-        }
+        //Serialization to send result back to MainActivity
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ResultMarker, result_);
+        Intent intent = new Intent();
+        intent.putExtras(bundle);
+        setResult( 0, intent);
     }
 
     @Override
@@ -258,50 +129,131 @@ public class SelectItemActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.add_item ) {
-            adapter_.items_.add( "" );
-            adapter_.selected_ = adapter_.getCount() - 1;
-            adapter_.notifyDataSetChanged();
-        } else if ( id == R.id.validate_items ){
-            updateResult();
-            finish();
+            adapter_.newItem();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    void updateResult(){
-        //Compute the modified and added list
-        for ( int i = 0; i < input_.values_.size(); i++ ) {
-            String oldItem = input_.values_.get( i );
-            String newItem = adapter_.getItem(i);
-            if ( !oldItem.equals(newItem) ) {
-                result_.modifiedItems_.add( new ValueChange( oldItem, newItem ) );
+    class SelectItemAdapter extends RecyclerView.Adapter<SelectItemAdapter.SelectItemViewHolder> {
+        final ArrayList<String> items_ = new ArrayList<>();
+        SelectItemActivity activity_;
+        int selected_ = 0;
+
+        class SelectItemViewHolder extends RecyclerView.ViewHolder{
+            RadioButton selector_;
+            EditText text_;
+            ImageButton editButton_;
+            ImageButton removeButton_;
+            SelectItemViewHolder(final View itemView) {
+                super(itemView);
+                selector_     = itemView.findViewById(R.id.item_selector);
+                text_         = itemView.findViewById(R.id.item_text);
+                editButton_   = itemView.findViewById(R.id.edit_item);
+                removeButton_ = itemView.findViewById(R.id.remove_item);
+                text_.addTextChangedListener( new TextWatcher() {
+                    @Override public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                        items_.set( getAdapterPosition(), charSequence.toString() );
+                    }
+
+                    @Override public void afterTextChanged(Editable editable) {}
+                });
+                selector_.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selected_ = getAdapterPosition();
+                        finish();
+                    }
+                });
+                removeButton_.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Remove it from the lists
+                        int positionRemoved = getAdapterPosition();
+                        if ( positionRemoved < activity_.input_.values_.size() ) {
+                            activity_.result_.removedItems_.add(activity_.input_.values_.get( positionRemoved ));
+                            activity_.input_.values_.remove(positionRemoved);
+                        }
+                        items_.remove(positionRemoved);
+                        notifyItemRemoved(positionRemoved);
+
+                        //Update the selected item if it was after, after a delay to have the remove item animation
+                        if ( positionRemoved <= selected_ && selected_ > 0 ) {
+                            listView_.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    selected_ = selected_ - 1;
+                                    notifyItemChanged(selected_);
+                                }
+                            }, 200);
+                        }
+                    }
+                });
+
+                editButton_.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        InputMethodManager mgr = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        text_.requestFocus();
+                        text_.setSelection( text_.getText().toString().length() );
+                        mgr.showSoftInput(text_, InputMethodManager.SHOW_IMPLICIT);
+                    }
+                });
             }
         }
 
-        for ( int i = input_.values_.size(); i < adapter_.getCount(); i++ ) {
-            result_.addedItems_.add( adapter_.getItem(i));
-        }
-        result_.selected_ = adapter_.getItem( adapter_.selected_ );
-
-        //Deep copy the result
-        Result result = null;
-        try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(baos);
-            oos.writeObject(result_);
-            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-            ObjectInputStream ois = new ObjectInputStream(bais);
-            result = (Result) ois.readObject();
-        } catch ( IOException|ClassNotFoundException ex ) {
-            result = result_;
+        SelectItemAdapter(SelectItemActivity activity){
+            activity_ = activity;
+            items_.addAll(activity.input_.values_);
+            for ( int i = 0; i < items_.size(); i++ ) {
+                if ( items_.get(i).equals( activity.input_.selected_ ) ) {
+                    selected_ = i;
+                }
+            }
         }
 
-        //Serialization to send result back to MainActivity
-        Bundle bundle = new Bundle();
-        bundle.putSerializable(ResultMarker, result);
-        Intent intent = new Intent();
-        intent.putExtras(bundle);
-        setResult( 0, intent);
+        @Override
+        public int getItemCount() {
+            return items_.size();
+        }
+
+        @Override @NonNull
+        public SelectItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.select_item,parent, false);
+            return new SelectItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull final SelectItemViewHolder holder, int position) {
+            holder.text_.setText(items_.get(position));
+            holder.selector_.setChecked( position == selected_ );
+        }
+
+        void newItem() {
+            items_.add( "" );
+            final int lastPosition = adapter_.getItemCount() - 1;
+            adapter_.notifyItemInserted(lastPosition);
+
+            //Post scroll event to the last item after update is done
+            listView_.post( new Runnable() {
+                @Override public void run() {
+                    final LinearLayoutManager layout = (LinearLayoutManager) listView_.getLayoutManager();
+                    layout.scrollToPosition( lastPosition );
+
+                    //Post click event after the scroll has been completed
+                    listView_.post( new Runnable() {
+                        @Override public void run() {
+                            int firstVisible = layout.findFirstVisibleItemPosition();
+                            int lastVisible = layout.findLastVisibleItemPosition();
+                            System.out.println("First " + firstVisible + " last " + lastVisible + " pour position " + lastPosition);
+                            View view = listView_.getChildAt(lastVisible - firstVisible);
+                            view.findViewById(R.id.edit_item).callOnClick();
+                        }
+                    });
+                }
+            });
+        }
     }
 }
